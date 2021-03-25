cec0a189538006c79c78128be6fbc32be8d26280
koichirokudo <koichirokudo746@gmail.com>
2021-03-25 21:49:22 +0900

    save name by enter key

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -73,6 +73,12 @@ export default function SignIn({ setName }) {
             label="ニックネーム"
             autoFocus
             onChange={(e) => setString(e.target.value)}
+            onKeyDown={(e) => {
+              if(e.key === 'Enter') {
+                setName(e.target.value);
+                e.preventDefault();
+              }
+            }}
           />
           <Button
             type="button"
