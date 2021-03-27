a1dee744d1313523486f3d15758672b7de498f17
koichirokudo <koichirokudo746@gmail.com>
2021-03-27 11:11:57 +0900

    create TextField component

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { gravatarPath } from '../gravatar';
+import MessageField from './MessageField';
 
 const useStyles = makeStyles({
   root: {
@@ -9,7 +10,9 @@ const useStyles = makeStyles({
     margin: '26px',
   },
 });
+
 const MessageInputField = ({ name }) => {
+  const [text, setText] = useState('');
   const classes = useStyles();
   const avatarPath = gravatarPath(name);
 
@@ -19,7 +22,9 @@ const MessageInputField = ({ name }) => {
         <Grid xs={1}>
           <Avatar src={avatarPath} />
         </Grid>
-        <Grid xs={10}>入力</Grid>
+        <Grid xs={10}>
+          <MessageField name={name} setText={setText} text={text} />
+        </Grid>
         <Grid xs={1}>ボタン</Grid>
       </Grid>
     </div>
