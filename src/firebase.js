5457c29f282d8e34f16291ac9b9373be3c6d6be9
koichirokudo <koichirokudo746@gmail.com>
2021-03-27 15:46:19 +0900

    create app

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAsa2eymZ-WeSZiISLYOqOd4K4qiCb-Y6Y',
+  authDomain: 'idobatakaigi-with-koichiro.firebaseapp.com',
+  projectId: 'idobatakaigi-with-koichiro',
+  storageBucket: 'idobatakaigi-with-koichiro.appspot.com',
+  messagingSenderId: '250418924926',
+  appId: '1:250418924926:web:0762f9467a454addc2772f',
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref('messages');
+
+export const pushMessage = ({ name, text }) => {
+  messageRef.push({ name, text });
+};
